77242b5b2c727281b4b369fdebbe557ff87929bb
aanchalsaket <aanchalsaketggits@gmail.com>
2024-08-31 15:18:23 +0530

    add 3 num

---
+++ Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        int num3 = 4;
+        int sum = num1 + num2 + num3;
+        System.out.print("The sum is : ");
+        System.out.println(sum);
+    }
+}
